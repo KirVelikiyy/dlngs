a98191bebdeed9e3ff702e7dd77f7cdee042cb99
KirVelikiy <lisov.kirill50@gmail.com>
2023-10-14 21:49:07 +0400

    add main app's model

--- dlngs/models.py
+++ dlngs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Dictionary(models.Model):
+    name = models.CharField(max_length=30)
+    data = models.TextField(max_length=1000)
+    holder = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='dictionaries'
+    )
